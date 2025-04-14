fad64cb472b903e7c507692ab3ba94f135b295f6
rogue-clown <119892557+rogue-clown@users.noreply.github.com>
2025-04-14 09:32:17 -0500

    Valedictory sponsor scrubber

--- workfixer.js
+++ workfixer.js
@@ -196,6 +196,7 @@ export function workFixer(worksPull) {
     fixedWorks = fixedWorks.replace(/George E. Mitchell Black-Eyed Susan/ig, "Black-Eyed Susan");
     fixedWorks = fixedWorks.replace(/B. Wayne Hughes Beholder Mile/ig, "Beholder Mile");
     fixedWorks = fixedWorks.replace(/Michelob Ultra Challenger/ig, "Challenger");
+    fixedWorks = fixedWorks.replace(/bet365 Valedictory/ig, "Valedictory");
 
     // common sponsor names and "presented by" blocks
 
